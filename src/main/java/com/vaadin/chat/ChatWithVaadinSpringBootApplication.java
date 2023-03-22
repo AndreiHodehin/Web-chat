3c6583fe2a06216425e0fbf1f095e58af26d6b6e
Andrei <chodechin00@gmail.com>
2023-03-22 14:51:45 +0200

    first commit

---
+++ src/main/java/com/vaadin/chat/ChatWithVaadinSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vaadin.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWithVaadinSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWithVaadinSpringBootApplication.class, args);
+    }
+
+}
